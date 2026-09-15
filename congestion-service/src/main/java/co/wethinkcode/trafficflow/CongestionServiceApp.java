487be7f00ae04a0af7afff8a83b05c571e7cb2f4
divine <divinelubiku6@gmail.com>
2026-09-16 01:26:55 +0200

    feat: route endpoint exposure

--- congestion-service/src/main/java/co/wethinkcode/trafficflow/CongestionServiceApp.java
+++ congestion-service/src/main/java/co/wethinkcode/trafficflow/CongestionServiceApp.java
@@ -2,19 +2,29 @@ package co.wethinkcode.trafficflow;
 
 import io.javalin.Javalin;
 
+import java.util.Map;
+
 public class CongestionServiceApp {
 
-    private int congestionLevel = 3;
+    private static int congestionLevel = 3;
 
-
-    private static void getCongestionLevel() {
-
-    }
 
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(7022);
 
-        app.get("/health", ctx -> ctx.result("OK"));
+        app.get("/congestion", ctx -> ctx.json(Map.of("level", congestionLevel))); // crerate key-val pair of congest lvl
+
+        app.post("/congestion", ctx -> {
+            int level = ctx.queryParamAsClass("level", Integer.class).getOrDefault(congestionLevel); // search for lvl: and val of level to use
+
+            if (level < 0 || level > 8) {
+                ctx.status(400).result("Level must be between 0 & 8 ");
+                return;
+            }
+
+            congestionLevel = level;
+            ctx.json(Map.of("level", congestionLevel));
+        });
 
 
         // TODO (Tracks the city-wide Congestion Level (0-8).)
