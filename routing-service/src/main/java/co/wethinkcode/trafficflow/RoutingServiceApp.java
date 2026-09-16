90cf13e8ed1f2e6ae42a0e42241b1a710849bd4e
divine <divinelubiku6@gmail.com>
2026-09-16 02:21:29 +0200

    feat:Made route from intersection records

--- routing-service/src/main/java/co/wethinkcode/trafficflow/RoutingServiceApp.java
+++ routing-service/src/main/java/co/wethinkcode/trafficflow/RoutingServiceApp.java
@@ -2,15 +2,33 @@ package co.wethinkcode.trafficflow;
 
 import io.javalin.Javalin;
 
+import java.util.Map;
+
 public class RoutingServiceApp {
 
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(7023);
 
         app.get("/health", ctx -> ctx.result("OK"));
+        app.get("/route", ctx -> {
 
-        // TODO (Provides estimated travel times based on congestion and intersection.)
-        // Add domain endpoints for routing-service here.
+            String fromID = ctx.queryParam("from");
+            String toID = ctx.queryParam("to");
+
+            if (fromID == null || toID == null) {
+                ctx.status(400).result("Bothvalues are needed");
+                return;
+            }
+
+            ctx.json(Map.of(
+                    "from", fromID,
+                    "to", toID,
+                    "status", "found_route"
+            ));
+
+            // TODO (Provides estimated travel times based on congestion and intersection.)
+            // Add domain endpoints for routing-service here.
+        });
     }
 }
 
