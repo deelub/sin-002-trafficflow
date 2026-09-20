49ce836f485e39416fbce9e9f721d080bf0a0d30
divine <divinelubiku6@gmail.com>
2026-09-20 18:28:19 +0200

    feat : add publisher for congestion messages

--- routing-service/src/main/java/co/wethinkcode/trafficflow/mq/MqConfig.java
+++ routing-service/src/main/java/co/wethinkcode/trafficflow/mq/MqConfig.java
@@ -1,5 +1,9 @@
 package co.wethinkcode.trafficflow.mq;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
 /**
  * Shared by every producer/consumer service that talks to the "congestion-topic"
  * ActiveMQ topic. Duplicated into each participating service's own source tree,
@@ -11,5 +15,30 @@ public final class MqConfig {
     public static final String TOPIC = "congestion-topic";
 
     private MqConfig() {
+    }
+
+    public static void publishUpdate(int level){
+
+        ActiveMQConnectionFactory connectionFactory= new ActiveMQConnectionFactory(BROKER_URL);
+
+        try{
+            Connection connection = connectionFactory.createConnection();
+            connection.start();
+
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            Topic topic = session.createTopic("congestion-topic");
+
+            MessageProducer producer = session.createProducer(topic);
+
+            TextMessage message = session.createTextMessage("level" + level);
+            producer.send(message);
+
+            session.close();
+            connection.close();
+
+        }catch(JMSException e){
+            e.printStackTrace();
+        }
     }
 }
